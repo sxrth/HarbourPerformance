d4db95f4d0d31bf200ca48c985b1373b5108fa4a
sxrth <ismailtopcu620@gmail.com>
2026-08-31 11:13:26 +0300

    Add files via upload

--- src/main/java/com/sxrth/obsidian/ClientEntrypoint.java
+++ src/main/java/com/sxrth/obsidian/ClientEntrypoint.java
@@ -1,10 +1,17 @@
 package com.sxrth.obsidian;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 
 public final class ClientEntrypoint implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ObsidianOptimization.initialize();
+        FabricLoader loader = FabricLoader.getInstance();
+
+        System.out.println("[ObsidianOptimization] 1.6.5 loaded.");
+        System.out.println("[ObsidianOptimization] Sodium=" + loader.isModLoaded("sodium"));
+        System.out.println("[ObsidianOptimization] Iris=" + loader.isModLoaded("iris"));
+        System.out.println("[ObsidianOptimization] Lithium=" + loader.isModLoaded("lithium"));
+        System.out.println("[ObsidianOptimization] ImmediatelyFast=" + loader.isModLoaded("immediatelyfast"));
     }
 }
